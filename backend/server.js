a972d1faabc8f7043b146b347eac10d000168501
Robert <109326004+rdcheadle3@users.noreply.github.com>
2023-06-30 15:49:13 -0400

    PD-004b: Create Book catalog component
    Design and implement the frontend views to display
    the list of available books using React components.
    •	Create a new React component Catalog.js, to display
      the list of books.
    •	Fetch book data from the backend using express
    •	Use React state or state management libraries to
      store and manage the fetched book data.
    •	Design the catalog view using HTML and CSS within
      the React component.
    •	Implement filtering and sorting options in the frontend
      to facilitate book search and display the filtered results.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app = express();
-const authRoutes = require('./backend/authRoutes');
+const { Pool } = require('pg');
 
-// Other server configuration and middleware
+// Create a new pool for connecting to the PostgreSQL database
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'bookstore_database',
+  password: '#Bobc0151#',
+  port: 5432, // Replace with your database port if necessary
+});
 
-// Use the authRoutes
-app.use('/api/auth', authRoutes);
+// Enable CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
-// Other routes and middleware
+// Set up middleware to parse JSON request bodies
+app.use(express.json());
+
+// Define a route to fetch all books
+app.get('/api/books', async (req, res) => {
+  try {
+    // Query the database to fetch all books
+    const { rows } = await pool.query('SELECT * FROM catalog');
+
+    // Return the books as a JSON response
+    res.json(rows);
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
+});
 
 // Start the server
-app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+const port = 8000; // Choose a port number
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
