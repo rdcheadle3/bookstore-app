c183670f450bda9da06c54b37df12b3941c5a718
Robert <109326004+rdcheadle3@users.noreply.github.com>
2023-06-29 22:41:07 -0400

    PD-002: add validation to forms using formik and yup libraries. Update form components to include validation

--- frontend/bookstore-app/src/components/LoginForm.js
+++ frontend/bookstore-app/src/components/LoginForm.js
@@ -1,22 +1,50 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import './forms.css';
 
 const LoginForm = () => {
+  const initialValues = {
+    username: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    // Handle form submission logic here
+    console.log(values);
+    resetForm();
+  };
+
   return (
     <div>
       <h2>Login Form</h2>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <Field type="text" id="username" name="username" />
+            <ErrorMessage name="username" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Field type="password" id="password" name="password" />
+            <ErrorMessage name="password" component="div" className="error" />
+          </div>
+          <button type="submit">Login</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
 
 export default LoginForm;
+
--- frontend/bookstore-app/src/components/RegisterForm.js
+++ frontend/bookstore-app/src/components/RegisterForm.js
@@ -1,26 +1,56 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import './forms.css';
 
 const RegisterForm = () => {
+  const initialValues = {
+    username: '',
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    // Handle form submission logic here
+    console.log(values);
+    resetForm();
+  };
+
   return (
     <div>
       <h2>Registration Form</h2>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <Field type="text" id="username" name="username" />
+            <ErrorMessage name="username" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Field type="password" id="password" name="password" />
+            <ErrorMessage name="password" component="div" className="error" />
+          </div>
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
 
-export default RegisterForm;
+export default Register
