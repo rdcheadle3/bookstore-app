f744da7b1ddd24d91f7565c5788c15068b6e3a8a
Robert <109326004+rdcheadle3@users.noreply.github.com>
2023-06-29 21:54:35 -0400

    PD-001: Base project structure set up,
    created basic registration form and login components

---
+++ frontend/bookstore-app/src/components/RegisterForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterForm = () => {
+  return (
+    <div>
+      <h2>Registration Form</h2>
+      {/* Add form elements here */}
+    </div>
+  );
+};
+
+export default RegisterForm;
